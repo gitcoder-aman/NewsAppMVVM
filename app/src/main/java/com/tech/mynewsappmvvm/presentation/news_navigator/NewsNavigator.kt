290bd5d4b4be8d9cf6d4c23c282ad6dffc9d4289
Aman Kumar <amankumar93578@gmail.com>
2024-04-27 11:05:46 +0530

    done bottom navigation bar

---
+++ app/src/main/java/com/tech/mynewsappmvvm/presentation/news_navigator/NewsNavigator.kt
@@ -0,0 +1,203 @@
+package com.tech.mynewsappmvvm.presentation.news_navigator
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.tech.mynewsappmvvm.R
+import com.tech.mynewsappmvvm.domain.model.Article
+import com.tech.mynewsappmvvm.presentation.bookmark.BookmarkScreen
+import com.tech.mynewsappmvvm.presentation.bookmark.BookmarkViewModel
+import com.tech.mynewsappmvvm.presentation.details.DetailScreen
+import com.tech.mynewsappmvvm.presentation.details.DetailViewModel
+import com.tech.mynewsappmvvm.presentation.details.DetailsEvent
+import com.tech.mynewsappmvvm.presentation.home.HomeScreen
+import com.tech.mynewsappmvvm.presentation.home.HomeViewModel
+import com.tech.mynewsappmvvm.presentation.news_navigator.components.BottomNavigationItem
+import com.tech.mynewsappmvvm.presentation.news_navigator.components.NewsBottomNavigation
+import com.tech.mynewsappmvvm.presentation.nvgraph.Route
+import com.tech.mynewsappmvvm.presentation.search.SearchScreen
+import com.tech.mynewsappmvvm.presentation.search.SearchViewModel
+
+@Composable
+fun NewsNavigator() {
+    val bottomNavigationItems = remember {
+        listOf(
+            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
+            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark")
+        )
+    }
+    val navController = rememberNavController()
+    val backStackState = navController.currentBackStackEntryAsState().value
+    var selectedItem by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    selectedItem = remember(key1 = backStackState) {
+        when (backStackState?.destination?.route) {
+            Route.HomeScreen.route -> 0
+            Route.SearchScreen.route -> 1
+            Route.BookmarkScreen.route -> 2
+            else -> 0
+        }
+    }
+    val isBottomBarVisible = remember(key1 = backStackState) {
+        backStackState?.destination?.route == Route.HomeScreen.route ||
+                backStackState?.destination?.route == Route.SearchScreen.route ||
+                backStackState?.destination?.route == Route.BookmarkScreen.route
+    }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            if (isBottomBarVisible) {
+                NewsBottomNavigation(
+                    items = bottomNavigationItems,
+                    selected = selectedItem,
+                    onItemClick = { index ->
+                        when (index) {
+                            0 -> navigateToTab(
+                                navController = navController,
+                                route = Route.HomeScreen.route
+                            )
+
+                            1 -> navigateToTab(
+                                navController = navController,
+                                route = Route.SearchScreen.route
+                            )
+
+                            2 -> navigateToTab(
+                                navController = navController,
+                                route = Route.BookmarkScreen.route
+                            )
+                        }
+                    })
+            }
+        }
+    ) {
+        val bottomPadding = it.calculateBottomPadding()
+        NavHost(
+            navController = navController,
+            startDestination = Route.HomeScreen.route,
+            modifier = Modifier.padding(bottom = bottomPadding)
+        ) {
+            composable(route = Route.HomeScreen.route) {
+                val viewModel: HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = articles, navigateToSearch = {
+                    navigateToTab(
+                        navController = navController,
+                        route = Route.SearchScreen.route
+                    )
+                }, navigateToDetails = { article ->
+                    navigateToDetails(
+                        navController = navController,
+                        article = article
+                    )
+                })
+            }
+            composable(route = Route.SearchScreen.route) {
+                val viewModel: SearchViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                SearchScreen(
+                    state = state,
+                    event = viewModel::onEvent,
+                    navigateToDetails = { article ->
+                        navigateToDetails(
+                            navController = navController,
+                            article = article
+                        )
+                    })
+            }
+            composable(route = Route.DetailsScreen.route) {
+                val viewModel: DetailViewModel = hiltViewModel()
+                //TODO : Handle Side Effect
+                if(viewModel.sideEffect != null){
+                    Toast.makeText(LocalContext.current, viewModel.sideEffect, Toast.LENGTH_SHORT).show()
+                    viewModel.onEvent(DetailsEvent.RemoveSideEffect)
+                }
+                navController.previousBackStackEntry?.savedStateHandle?.get<Article?>("article")
+                    ?.let { article ->
+                        DetailScreen(article = article, event = viewModel::onEvent, navigationUp = {
+                            navController.navigateUp()
+                        })
+                    }
+            }
+            composable(route = Route.BookmarkScreen.route) {
+                val viewModel: BookmarkViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                BookmarkScreen(state = state, navigateToDetails = { article ->
+                    navigateToDetails(
+                        navController = navController,
+                        article = article
+                    )
+                })
+            }
+        }
+    }
+}
+
+private fun navigateToTab(navController: NavController, route: String) {
+    navController.navigate(route) {
+        navController.graph.startDestinationRoute?.let { homeScreen ->
+            popUpTo(homeScreen) {
+                saveState = true
+            }
+            restoreState = true
+            launchSingleTop = true
+        }
+    }
+}
+
+private fun navigateToDetails(navController: NavController, article: Article) {
+    navController.currentBackStackEntry?.savedStateHandle?.set("article", article)
+    navController.navigate(
+        route = Route.DetailsScreen.route
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/tech/mynewsappmvvm/presentation/nvgraph/NavGraph.kt
+++ app/src/main/java/com/tech/mynewsappmvvm/presentation/nvgraph/NavGraph.kt
@@ -1,33 +1,14 @@
 package com.tech.mynewsappmvvm.presentation.nvgraph
 
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import androidx.navigation.navigation
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.tech.mynewsappmvvm.domain.model.Article
-import com.tech.mynewsappmvvm.domain.model.Source
-import com.tech.mynewsappmvvm.domain.model.toEncodedString
-import com.tech.mynewsappmvvm.presentation.bookmark.BookmarkScreen
-import com.tech.mynewsappmvvm.presentation.bookmark.BookmarkViewModel
-import com.tech.mynewsappmvvm.presentation.details.DetailScreen
-import com.tech.mynewsappmvvm.presentation.home.HomeScreen
-import com.tech.mynewsappmvvm.presentation.home.HomeViewModel
+import com.tech.mynewsappmvvm.presentation.news_navigator.NewsNavigator
 import com.tech.mynewsappmvvm.presentation.onboarding.OnBoardingScreen
 import com.tech.mynewsappmvvm.presentation.onboarding.OnBoardingViewModel
-import com.tech.mynewsappmvvm.presentation.search.SearchScreen
-import com.tech.mynewsappmvvm.presentation.search.SearchViewModel
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import java.net.URLDecoder
-import java.net.URLEncoder
 
 @Composable
 fun NavGraph(
@@ -56,28 +37,7 @@ fun NavGraph(
             composable(
                 route = Route.NewsNavigationScreen.route
             ) {
-                val viewModel: BookmarkViewModel = hiltViewModel()
-                BookmarkScreen(state = viewModel.state.value) {
-
-                }
-            }
-        }
-//        composable(
-//            route = "${Route.DetailsScreen.route}/{data}",
-//            arguments = listOf(navArgument("data") {
-//                type = NavType.StringType
-//            })
-//        ) { backStackEntry ->
-//            val data = backStackEntry.arguments?.getString("data")
-//            val article = data?.let { Article.fromEncodedString(it) }
-//            DetailScreen(article = article!!, event = {}) {
-//                navController.navigateUp()
-//            }
-//        }
-        composable(route = Route.SearchScreen.route) {
-            val viewModel: SearchViewModel = hiltViewModel()
-            SearchScreen(state = viewModel.state.value, event = viewModel::onEvent) {
-                navController.navigate(it)
+                NewsNavigator()
             }
         }
     }
